c822e333db1e2c70828a9d2b0a1431c47a1b306d
choi yunseo <chldbstj0512@ajou.ac.kr>
2022-12-27 14:59:55 +0900

    fetch

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const APIKEY = `4112003d49d61c770af01c3a817c8dc3`;
+  console.log(data);
+
+  useEffect(() => {
+    fetch(`https://dapi.kakao.com/v2/local/search/address.json?query=전북 삼성동 100`, {
+      headers: {
+        Authorization: `KakaoAK ${APIKEY}`
+      }})
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <Text>Loading...</Text> : 
+      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
+          <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
+          <FlatList
+            data={data.articles}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.id + '. ' + item.title}</Text>
+            )}
+          />
+        </View>
+      )}
+    </View>
+  );
+};
