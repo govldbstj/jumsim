ec5607dddaa30d7872db9a22242aebd2bcfd7f42
choi yunseo <chldbstj0512@ajou.ac.kr>
2022-12-27 16:37:21 +0900

    cafemode

--- App.js
+++ App.js
@@ -1,39 +1,75 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, Text, View, StyleSheet, TextInput } from 'react-native';
 
 export default App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  const APIKEY = `4112003d49d61c770af01c3a817c8dc3`;
-  console.log(data);
 
-  useEffect(() => {
-    fetch(`https://dapi.kakao.com/v2/local/search/address.json?query=전북 삼성동 100`, {
-      headers: {
-        Authorization: `KakaoAK ${APIKEY}`
-      }})
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
+  const [menu, setMenu] = useState('');
+  const [data, setData] = useState([]);
+
+  const handlemenu = menu => {
+    setMenu(menu);
+  }
+
+  const searchMenu = () => {
+    
+    console.log("ok")
+    const APIKEY = `4112003d49d61c770af01c3a817c8dc3`;
+    console.log(data);
+
+    let xcoord = 127.06;
+    let ycoord = 37.51;
+    let radius = 20000;
+
+    let cafe = 'CE7';
+
+    useEffect(() => {
+      fetch(`https://dapi.kakao.com/v2/local/search/category.json?category\_group\_code=${cafe}&y=${ycoord}&x=${xcoord}&radius=${radius}`, {
+        headers: {
+          Authorization: `KakaoAK ${APIKEY}`
+        }})
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }, []);
+
+  }
 
   return (
+    searchMenu(),
+    <View style={styles.view}>
+      <Text style = {styles.text}>점심 메뉴 추천</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="메뉴 키워드를 입력하세요"
+          autoCorrect={false}
+          value={menu}
+          onChangeText={handlemenu}
+          onSubmitEditing={()=>setMenu}
+          multiline={false}
+          returnKeyType="search"
+        />
+        <Text>{menu}</Text>
+      </View>
+    );
+  };
 
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <Text>Loading...</Text> : 
-      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
-          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.title}</Text>
-          <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
-          <FlatList
-            data={data.articles}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <Text>{item.id + '. ' + item.title}</Text>
-            )}
-          />
-        </View>
-      )}
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+    input: {
+      height: 42,
+      width: 250,
+      margin: 12,
+      borderWidth: 1,
+      borderRadius: 5,
+      backgroundColor: "#FFFFFF",
+      padding: 10,
+    },
+    view: {
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 30,
+        marginTop: 60,
+        margintBottom: 30
+    }
+});
